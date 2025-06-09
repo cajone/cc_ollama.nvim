518a1f8ca81cd3e86f6b7f48e831a95d7b0653d6
cajone <peat@blueyonder.co.uk>
2025-06-09 22:40:37 +0100

    Added init.lua to providers

---
+++ lua/codecompanion/providers/init.lua
@@ -0,0 +1,6 @@
+-- lua/codecompanion/providers/init.lua
+-- Minimal file to satisfy `require("codecompanion.providers")`
+-- This file is intentionally empty to prevent loading complex UI providers
+-- while allowing the core plugin to initialize.
+
+return {}
